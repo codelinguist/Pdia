37431cacfd9222d07a48efd0cd985f137f3489e4
Ralph Jourdan Barro <ralph@creativegurus.com>
2015-08-15 18:43:13 +0800

    Solution Fix

---
+++ src/Pdia.Services/ServiceLayerModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using Pdia.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Services
+{
+    public class ServiceLayerModule : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
