b594a0e7996514492fda98ba5b9d9a3c146501e6
Daniel Seville <danielcseville@gmail.com>
2015-08-16 00:44:30 +0800

    added patient repository

---
+++ src/Pdia.Infrastructure/IPatientService.cs
@@ -0,0 +1,17 @@
+﻿using Pdia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Infrastructure
+{
+    public interface IPatientService
+    {
+        Task<Patient> FindAsync(Guid Id);
+        Task<Patient> InsertAsync(Patient patient);
+        Task<Patient> UpdateAsync(Patient patient);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ src/Pdia.Services/PatientService.cs
@@ -0,0 +1,60 @@
+﻿using Pdia.Entities;
+using Pdia.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Services
+{
+    public class PatientService : IPatientService
+    {
+        IPdiaUnitOfWorkFactory _uowFac;
+        public PatientService(IPdiaUnitOfWorkFactory uowFac)
+        {
+            _uowFac = uowFac;
+        }
+
+        public async Task<Patient> FindAsync(Guid Id)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                return (await uow.PatientRepository.ItemsAsync(q => q.Id == Id)).FirstOrDefault();
+            }
+        }
+
+        public async Task<Patient> InsertAsync(Patient patient)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                patient.Id = new Guid();
+
+                uow.PatientRepository.Insert(patient);
+                await uow.SaveChangesAsync();
+
+                return patient;
+            }
+        }
+
+        public async Task<Patient> UpdateAsync(Patient patient)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                uow.PatientRepository.Update(patient);
+                await uow.SaveChangesAsync();
+
+                return patient;
+            }
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                uow.PatientRepository.Delete(id);
+                await uow.SaveChangesAsync();
+            }
+        }
+    }
+}
