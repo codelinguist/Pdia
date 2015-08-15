1048c014f1a25a4cd4021aef3fc877f8beba88da
Daniel Seville <danielcseville@gmail.com>
2015-08-16 00:47:41 +0800

    added post service

---
+++ src/Pdia.Infrastructure/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using Pdia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Infrastructure
+{
+    public interface IPostService
+    {
+        Task<Post> FindAsync(Guid Id);
+        Task<Post> InsertAsync(Post post);
+        Task<Post> UpdateAsync(Post post);
+        Task DeleteAsync(Post post);
+    }
+}
---
+++ src/Pdia.Services/PostService.cs
@@ -0,0 +1,62 @@
+﻿using Pdia.Entities;
+using Pdia.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Services
+{
+    public class PostService : IPostService
+    {
+        IPdiaUnitOfWorkFactory _uowFac;
+        public PostService(IPdiaUnitOfWorkFactory uowFac)
+        {
+            _uowFac = uowFac;
+        }
+
+        public async Task<Post> FindAsync(Guid Id)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                return (await uow.PostRepository.ItemsAsync(q => q.Id == Id)).FirstOrDefault();
+            }
+        }
+
+        public async Task<Post> InsertAsync(Post post)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                post.Id = new Guid();
+
+                uow.PostRepository.Insert(post);
+                await uow.SaveChangesAsync();
+
+                return post;
+            }
+        }
+
+        public async Task<Post> UpdateAsync(Post post)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                uow.PostRepository.Update(post);
+                await uow.SaveChangesAsync();
+
+                return post;
+            }
+        }
+
+        public async Task DeleteAsync(Post post)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                post.Deleted = true;
+
+                uow.PostRepository.Update(post);
+                await uow.SaveChangesAsync();
+            }
+        }
+    }
+}
