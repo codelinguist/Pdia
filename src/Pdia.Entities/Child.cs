01a066ebb034b862b1b763db5c7af3dd0fecac54
Ralph Jourdan Barro <ralph@creativegurus.com>
2015-08-15 18:08:37 +0800

    Initial Entities

---
+++ src/Pdia.Entities/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Entities
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+    public class Child
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+        public ICollection<UserProfile> Parents { get; set; }
+        public ICollection<Pediatrician> Pediatricians { get; set; }
+    }
+}
