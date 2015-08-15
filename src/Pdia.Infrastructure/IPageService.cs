ce2f0b8376d631c186d40abe31caa8aed740095c
Daniel Seville <danielcseville@gmail.com>
2015-08-16 00:34:22 +0800

    added page service

---
+++ src/Pdia.Infrastructure/IPageService.cs
@@ -0,0 +1,17 @@
+﻿using Pdia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Infrastructure
+{
+    public interface IPageService
+    {
+        Task<Page> FindAsync(Guid Id);
+        Task<Page> InsertAsync(Page page);
+        Task<Page> UpdateAsync(Page page);
+        Task DeleteAsync(Page page);
+    }
+}
---
+++ src/Pdia.Services/PageService.cs
@@ -0,0 +1,62 @@
+﻿using Pdia.Entities;
+using Pdia.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdia.Services
+{
+    public class PageService : IPageService
+    {
+        IPdiaUnitOfWorkFactory _uowFac;
+        public PageService(IPdiaUnitOfWorkFactory uowFac)
+        {
+            _uowFac = uowFac;
+        }
+
+        public async Task<Page> FindAsync(Guid Id)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                return (await uow.PageRepository.ItemsAsync(q => q.Id == Id)).FirstOrDefault();
+            }
+        }
+
+        public async Task<Page> InsertAsync(Page page)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                page.Id = new Guid();
+
+                uow.PageRepository.Insert(page);
+                await uow.SaveChangesAsync();
+
+                return page;
+            }
+        }
+
+        public async Task<Page> UpdateAsync(Page page)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                uow.PageRepository.Update(page);
+                await uow.SaveChangesAsync();
+
+                return page;
+            }
+        }
+
+        public async Task DeleteAsync(Page page)
+        {
+            using (var uow = _uowFac.Create())
+            {
+                page.Deleted = true;
+
+                uow.PageRepository.Update(page);
+                await uow.SaveChangesAsync();
+            }
+        }
+    }
+}
