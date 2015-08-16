80452ab0ba89dd243962799c618ed900a41e3bed
Ralph Jourdan Barro <codelinguist@gmail.com>
2015-08-16 10:24:16 +0800

    Notification Page

--- src-mobile/pdia/Pages/MenuPage.xaml.cs
+++ src-mobile/pdia/Pages/MenuPage.xaml.cs
@@ -11,6 +11,13 @@ namespace pdia
 		{
 			InitializeComponent (); 
 		}
+
+		void NotificationsTapped (object sender, EventArgs e)
+		{
+			var page = App.Instance.MainPage as MasterDetailPage;
+			page.Detail =new NavigationPage (new NotificationPage (){ }){ BarBackgroundColor = Color.FromHex ("#1ecaa7") };
+			page.IsPresented = false;
+		}
 	}
 }
 
